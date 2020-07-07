905a822113a254a3422090a290ac1b2e1c18856b
kimzhong <652269930@qq.com>
2020-07-07 17:02:41 +0800

    simpleui

--- lesson01/huangyu/devops10/settings.py
+++ lesson01/huangyu/devops10/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simpleui',
 
     'book.apps.BookConfig',
     'user.apps.UserConfig',
